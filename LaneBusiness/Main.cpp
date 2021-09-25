415f2ca48bef81f8cf1f9e5e98481a7aa1f761fa
Ilia Moiseev <villeman.5@yandex.ru>
2021-09-25 23:11:06 +0300

    Add 3DoF wrapper and define structure of the solution

--- LaneBusiness/Main.cpp
+++ LaneBusiness/Main.cpp
@@ -2,13 +2,44 @@
 #include "WSG84toCartesian.hpp"
 
 using namespace cv;
-using namespace wgs84;
+
+
+std::array<double, 3> toCartesian(std::array<double, 3> referencePoint, std::array<double, 3> targetPoint)
+{
+	/// Wrapper around similar function from namespace wgs84 which is 2DoF
+
+	std::array<double, 2> refPoint2DoF;
+	std::array<double, 2> targetPoint2DoF;
+	std::copy(referencePoint.begin(), referencePoint.begin() + 2, refPoint2DoF.begin());
+	std::copy(targetPoint.begin(), targetPoint.begin() + 2, targetPoint2DoF.begin());
+
+	std::array<double, 2> result2Dof = wgs84::toCartesian(refPoint2DoF, targetPoint2DoF);
+
+	std::array<double, 3> result = { 0., 0., 0. };
+	std::copy(result2Dof.begin(), result2Dof.begin() + 2, result.begin());
+
+	return result;
+}
 
 
 std::vector<int> busyLanes(std::array<double, 3> rPos, double rAzimuth,
 	std::array<double, 3> aPos, std::array<double, 3> bPos, int nLanes, double laneWidth,
 	std::array<double, 3> objPos, double objYaw, double objLength, double objWidth)
 {
+	//auto debug_result = wgs84::toCartesian(std::array<double, 2>({ 59.96769, 30.30985}), std::array<double, 2>({ 59.96783, 30.30958}));
+
+	// Find the road points in cartesian coords
+	std::array<double, 3> aPosCart = toCartesian(rPos, aPos);
+	std::array<double, 3> bPosCart = toCartesian(rPos, bPos);
+
+	// Rotate them to match robot's coordinate system
+
+	// Find all the points of the Lanes
+
+	// Find all the points of an object
+
+	// Check if any points of the object is inside any lane
+
 	return std::vector<int>(1, 2);
 }
 
@@ -30,7 +61,7 @@ bool test()
 
 	auto result = busyLanes(rPos, rAzimuth, aPos, bPos, nLanes, laneWidth, objPos, objYaw, objLength, objWidth);
 
-	if(result == std::vector<int>({1, 2}))
+	//if(result == std::vector<int>({1, 2}))
 	return true;
 }
 
