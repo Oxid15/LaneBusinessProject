00fd895c6e8291000e48301e7bf2ce5d76519964
Ilia Moiseev <villeman.5@yandex.ru>
2021-09-28 01:05:15 +0300

    Add new tests - third is not passed

--- LaneBusiness/Main.cpp
+++ LaneBusiness/Main.cpp
@@ -232,10 +232,60 @@ bool test()
 
 	if(result == std::vector<int>({1}))
 		return true;
+	else
+		return false;
+}
+
+bool test2()
+{
+	auto rPos = std::array<double, 3>({ 59.96769, 30.30985, 0. }); // lat, long, height
+	double rAzimuth = 30;
+
+	auto aPos = std::array<double, 3>({ 59.967681024726275, 30.310046939745639, 0. });
+	auto bPos = std::array<double, 3>({ 59.967752830076989, 30.310118554680990, 0. });
+	uint32_t nLanes = 2;
+	double laneWidth = 2.;
+
+	auto objPos = std::array<double, 3>({ 3., 9., 0. }); // y, x, z
+	double objYaw = 0.3235983;
+	double objLength = 4.;
+	double objWidth = 2.;
+
+	auto result = busyLanes(rPos, rAzimuth, aPos, bPos, nLanes, laneWidth, objPos, objYaw, objLength, objWidth);
+
+	if (result == std::vector<int>())
+		return true;
+	else
+		return false;
+}
+
+bool test3()
+{
+	auto rPos = std::array<double, 3>({ 59.96769, 30.30985, 0. }); // lat, long, height
+	double rAzimuth = 30;
+
+	auto aPos = std::array<double, 3>({ 59.967681024726275, 30.310046939745639, 0. });
+	auto bPos = std::array<double, 3>({ 59.967752830076989, 30.310118554680990, 0. });
+	uint32_t nLanes = 4;
+	double laneWidth = 2.;
+
+	auto objPos = std::array<double, 3>({ 7., 9., 0. }); // y, x, z
+	double objYaw = 0.3235983;
+	double objLength = 4.;
+	double objWidth = 2.;
+
+	auto result = busyLanes(rPos, rAzimuth, aPos, bPos, nLanes, laneWidth, objPos, objYaw, objLength, objWidth);
+
+	if (result == std::vector<int>())
+		return true;
+	else
+		return false;
 }
 
 int main()
 {
-	assert(test());
+	//assert(test());
+	//assert(test2());
+	assert(test3());
 	return 0;
 }
